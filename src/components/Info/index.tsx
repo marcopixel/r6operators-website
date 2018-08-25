0aad23286d7196f18d7205422e41c632a0158b5c
Marco Vockner <marcopixel@live.de>
2018-08-25 20:26:17 +0200

    update template file, fix typo

--- src/components/Info/index.tsx
+++ src/components/Info/index.tsx
@@ -49,11 +49,11 @@ export default class Info extends React.Component<any> {
                 </div>
                 <div className="info__divider" />
                 <div className="contribute">
-                    <div className="info__title">How can i help?</div>
+                    <div className="info__title">How can I help?</div>
                     <p>If you know Adobe Illustrator and want to help this project, please read the <a href="https://github.com/MarcoPixel/r6-operatoricons/blob/master/CONTRIBUTING.MD">guidelines for contributing</a> first. We're glad for every person who can help us :)</p>
                     <div className="info__title">Links</div>
                     <div className="contribute__links">
-                        <a href="https://github.com/MarcoPixel/r6-operatoricons">Github repo</a>
+                        <a href="https://github.com/MarcoPixel/r6-operatoricons">Github Repo</a>
                         <a href="https://github.com/MarcoPixel/r6-operatoricons/blob/master/LICENSE.MD">License</a>
                     </div>
                 </div>
