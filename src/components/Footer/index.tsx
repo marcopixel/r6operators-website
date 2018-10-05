2a62ded927f1f8d4992041ed94f1e5a5d441faad
Marco Vockner <marcopixel@live.de>
2018-10-05 19:31:12 +0200

    add attribution for @joeyfjj

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -6,7 +6,7 @@ export default class Footer extends React.Component<any> {
     render() {
         return (
             <div className="footer">
-                <span>Made with ❤️ by <a href="http://marcopixel.eu/">@marcopixel</a> & <a href="https://twitter.com/sniperdt">@dtSniper</a></span>
+                <span>Made with ❤️ by <a href="http://marcopixel.eu/">@marcopixel</a>, <a href="https://twitter.com/sniperdt">@dtSniper</a> & <a href="https://twitter.com/joeyfjj">@joeyfjj</a></span>
                 <div className="footer__legal">
                     <span>r6-operatoricons licensed under <a href="https://github.com/MarcoPixel/r6-operatoricons/blob/master/LICENSE.MD">Creative Commons Attribution 4.0</a></span>
                     <span>This site is not affiliated with Ubisoft Entertainment. Rainbow Six Siege is a registered trademark of Ubisoft.</span>
