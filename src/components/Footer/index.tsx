6e428c05cfd4b4d07d373a41f207a4ca24d0854d
Marco Vockner <marcopixel@live.de>
2019-03-11 00:46:30 +0100

    feat: add google analytics

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -10,6 +10,7 @@ export default class Footer extends React.Component<any> {
                 <div className="footer__legal">
                     <span>r6-operatoricons licensed under <a href="https://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0</a></span>
                     <span>This site is not affiliated with Ubisoft Entertainment. Rainbow Six Siege is a registered trademark of Ubisoft.</span>
+                    <span>This website uses anonymized Google Analytics to analyse traffic & improve our user experience in the future. <br/> You can opt-out by clicking <a href="javascript:gaOptout();">here</a>.</span>
                 </div>
             </div>
         );
