49485c7f1d084f9acf0a7bd70ae13b4d7637a472
Marco Vockner <marcopixel@live.de>
2019-04-27 20:21:38 +0200

    added in-progress zip generation script

--- scripts/buildZIP.js
+++ scripts/buildZIP.js
@@ -1,23 +1,65 @@
 const fs = require("fs");
+const path = require("path");
 const chalk = require('chalk');
-const svg_to_png = require("svg-to-png");
+const archiver = require('archiver');
 
 const config = require("./config.js");
 const testSource = require("./util/testSource.js");
 
-async function processSVG(array, src, dest, options) {
+function generateSingleZIP(fileName) {
+    return new Promise((resolve, reject) => {
+
+        //initialize archiver
+        const output = fs.createWriteStream(config.zipDestPath + `${fileName}.zip`);
+        const archive = archiver('zip', {
+            zlib: { level: 9 } // Sets the compression level.
+        });
+
+        // catch errors
+        output.on('error', error => {
+            if (error.code === 'ENOENT') {
+                reject(error)
+            } else {
+                reject(error);
+            }
+        });
+
+        // resolve on finish
+        output.on('finish', () => {
+            resolve(output);
+        });
+
+        // catch warnings (ie stat failures and other non-blocking errors)
+        archive.on('warning', error => {
+            reject(error);
+        });
+
+
+        // pipe archive data to the file
+        archive.pipe(output);
+
+        // add files to zip
+        archive.append(fs.createReadStream(`${config.svgPath}/${fileName}.svg`), { name: `${fileName}.svg` });
+        archive.append(fs.createReadStream(`${config.pngPath}/${fileName}.png`), { name: `${fileName}.png` });
+        archive.append(fs.createReadStream(`${config.aiPath}/${fileName}.ai`), { name: `${fileName}.ai` });
+
+        // finalize it and save it
+        archive.finalize();
+    })
+};
+
+async function processZIP(array, dest) {
 
     // set counter vars
     const inputCount = array.length;
     let outputCount = 0
 
     // inform the user that the process started
-    console.log(`${chalk.blue('info')} Starting ${chalk.bold('SVG to PNG')} conversion...`)
+    console.log(`${chalk.blue('info')} Starting ${chalk.bold('single icon ZIP')} generation...`)
 
-    // map the icon array and convert each icon independently
     var result = array.map(async item => {
         return await new Promise((resolve, reject) => {
-            svg_to_png.convert(src + "/" + item + ".svg", dest, options)
+            generateSingleZIP(item)
                 // success
                 .then(res => {
                     outputCount++;
@@ -28,14 +70,14 @@ async function processSVG(array, src, dest, options) {
                     rej => {
                         outputCount++;
                         console.log(`${"\n"}${chalk.red('error')} ${chalk.bold(outputCount)}/${chalk.bold(inputCount)} - ${item} failed!`)
-                        reject(item);
+                        reject(rej.stack);
                     })
                 // catch any unwanted errors
                 .catch(err => {
                     reject(err);
                 })
-        });
-    });
+        })
+    })
 
     // wait for all promises to finish
     Promise.all(result)
@@ -57,8 +99,8 @@ async function processSVG(array, src, dest, options) {
 }
 
 // check source files
-testSource(true, false, false).then(
-    res => { processSVG(config.iconArray, config.svgPath, config.pngDestPath, config.pngConfig); }, rej => {
+testSource(true, true, true).then(
+    res => { processZIP(config.iconArray, config.zipDestPath); }, rej => {
         console.error(`${chalk.red('error')} Source checks failed, aborting...`);
         return;
     })
