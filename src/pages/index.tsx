675820871143c82ae1d1d40d5865b63c8c1325b0
Marco Vockner <marcopixel@live.de>
2019-05-14 08:20:14 +0200

    fix deploy bug, disable reactGA debug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,15 +6,16 @@ import ReactGA from "react-ga";
 import { analytics } from "../../package.json";
 import "./base.scss";
 
-// init GA
-ReactGA.initialize(analytics.gaTrackingID, {
-    debug: analytics.debug,
-    titleCase: false,
-});
-ReactGA.set({ anonymizeIp: analytics.anonymizeIp });
-ReactGA.pageview(window.location.pathname + window.location.search);
-
 export default class IndexPage extends React.Component<any> {
+    componentDidMount() {
+        // init GA
+        ReactGA.initialize(analytics.gaTrackingID, {
+            debug: analytics.debug,
+            titleCase: false
+        });
+        ReactGA.set({ anonymizeIp: analytics.anonymizeIp });
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    }
     render() {
         return (
             <Layout>
