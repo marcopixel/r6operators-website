380fd5806aeea234b4d77083064c1af8ea5012a3
Marco Vockner <marcopixel@live.de>
2019-05-22 00:19:58 +0200

    add github shields to website

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,6 +3,7 @@ import { withPrefix } from "gatsby-link";
 import { version } from "../../../package.json";
 import ReactGA from "react-ga";
 import Icon, { ICON } from "components/Icon";
+import GitHubButton from "react-github-btn";
 
 import "./header.scss";
 
@@ -27,6 +28,18 @@ export default class Header extends React.Component<any> {
                 </div>
                 <div className="header__title">r6-operatoricons</div>
                 <div className="header__subtitle">Rainbow Six: Siege operator icons in SVG, PNG and AI.</div>
+                <div className="header__github">
+                    <img src="https://img.shields.io/github/last-commit/MarcoPixel/r6-operatoricons.svg?style=for-the-badge" />
+                    <a href="https://github.com/marcopixel/r6-operatoricons">
+                        <img src="https://img.shields.io/github/stars/MarcoPixel/r6-operatoricons.svg?style=for-the-badge" />
+                    </a>
+                    <a href="https://github.com/marcopixel/r6-operatoricons/network/members">
+                        <img src="https://img.shields.io/github/forks/MarcoPixel/r6-operatoricons.svg?style=for-the-badge" />
+                    </a>
+                    <a href="https://github.com/marcopixel/r6-operatoricons/subscription">
+                        <img src="https://img.shields.io/github/watchers/MarcoPixel/r6-operatoricons.svg?style=for-the-badge" />
+                    </a>
+                </div>
                 <div className="header__downloadAll">
                     <a
                         className="button secondary"
