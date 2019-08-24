9c35b325f3c3742a3a519a4970de232e1a9cf57f
Marco Vockner <marcopixel@live.de>
2019-08-25 00:29:54 +0200

    Add script to build icon object

---
+++ scripts/modules/buildIconObject.ts
@@ -0,0 +1,30 @@
+import path from "path";
+import { promises as fs } from "fs";
+
+import * as config from "../config";
+
+export default function buildIconsObject(array) {
+    return new Promise((resolve, reject) => {
+        const result = array.map(item => {
+            // eslint-disable-next-line promise/param-names
+            return new Promise((_resolve, _reject) => {
+                const name: string = item;
+                const filePath: string = path.join(`${config.sourcePath}${path.sep}${item}${path.sep}${item}.svg`);
+
+                // read file to get SVG content
+                fs.readFile(filePath, "utf8")
+                    .then(output => _resolve({ [name as string]: output as string })) // if successful, resolve with new array
+                    .catch(error => _reject(error)); // else reject with error
+            });
+        });
+
+        // wait for all icons to finish
+        Promise.all(result)
+            .then(resolved => {
+                // merge object containing arrays into one object
+                const merged = Object.assign({}, ...resolved);
+                return resolve(merged);
+            })
+            .catch(error => reject(error)); // reject error
+    });
+}
